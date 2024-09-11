5eb38f25eef9e4f1874f0da79eca0fac4f826671
zakinazzal <zakinazal112@gmail.com>
2024-09-11 18:25:59 +0300

    deleted:    ios/.gitignore 	deleted:    ios/Flutter/AppFrameworkInfo.plist 	deleted:    ios/Flutter/Debug.xcconfig 	new file:   ios/Flutter/Generated.xcconfig 	deleted:    ios/Flutter/Release.xcconfig 	new file:   ios/Flutter/flutter_export_environment.sh 	deleted:    ios/Podfile 	deleted:    ios/Runner.xcodeproj/project.pbxproj 	deleted:    ios/Runner.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	deleted:    ios/Runner.xcodeproj/project.xcworkspace/xcshareddata/GoogleService-Info.plist 	deleted:    ios/Runner.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist 	deleted:    ios/Runner.xcodeproj/project.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings 	deleted:    ios/Runner.xcodeproj/xcshareddata/xcschemes/Runner.xcscheme 	deleted:    ios/Runner.xcworkspace/contents.xcworkspacedata 	deleted:    ios/Runner.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist 	deleted:    ios/Runner.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings 	deleted:    ios/Runner/AppDelegate.swift 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Contents.json 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-1024x1024@1x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png 	deleted:    ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png 	deleted:    ios/Runner/Assets.xcassets/LaunchImage.imageset/Contents.json 	deleted:    ios/Runner/Assets.xcassets/LaunchImage.imageset/LaunchImage.png 	deleted:    ios/Runner/Assets.xcassets/LaunchImage.imageset/LaunchImage@2x.png 	deleted:    ios/Runner/Assets.xcassets/LaunchImage.imageset/LaunchImage@3x.png 	deleted:    ios/Runner/Assets.xcassets/LaunchImage.imageset/README.md 	deleted:    ios/Runner/Base.lproj/LaunchScreen.storyboard 	deleted:    ios/Runner/Base.lproj/Main.storyboard 	new file:   ios/Runner/GeneratedPluginRegistrant.h 	new file:   ios/Runner/GeneratedPluginRegistrant.m 	deleted:    ios/Runner/Info.plist 	deleted:    ios/Runner/Runner-Bridging-Header.h 	deleted:    ios/RunnerTests/RunnerTests.swift

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,98 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<cloud_firestore/FLTFirebaseFirestorePlugin.h>)
+#import <cloud_firestore/FLTFirebaseFirestorePlugin.h>
+#else
+@import cloud_firestore;
+#endif
+
+#if __has_include(<cloud_functions/FLTFirebaseFunctionsPlugin.h>)
+#import <cloud_functions/FLTFirebaseFunctionsPlugin.h>
+#else
+@import cloud_functions;
+#endif
+
+#if __has_include(<firebase_analytics/FLTFirebaseAnalyticsPlugin.h>)
+#import <firebase_analytics/FLTFirebaseAnalyticsPlugin.h>
+#else
+@import firebase_analytics;
+#endif
+
+#if __has_include(<firebase_app_check/FLTFirebaseAppCheckPlugin.h>)
+#import <firebase_app_check/FLTFirebaseAppCheckPlugin.h>
+#else
+@import firebase_app_check;
+#endif
+
+#if __has_include(<firebase_auth/FLTFirebaseAuthPlugin.h>)
+#import <firebase_auth/FLTFirebaseAuthPlugin.h>
+#else
+@import firebase_auth;
+#endif
+
+#if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
+#import <firebase_core/FLTFirebaseCorePlugin.h>
+#else
+@import firebase_core;
+#endif
+
+#if __has_include(<firebase_messaging/FLTFirebaseMessagingPlugin.h>)
+#import <firebase_messaging/FLTFirebaseMessagingPlugin.h>
+#else
+@import firebase_messaging;
+#endif
+
+#if __has_include(<firebase_storage/FLTFirebaseStoragePlugin.h>)
+#import <firebase_storage/FLTFirebaseStoragePlugin.h>
+#else
+@import firebase_storage;
+#endif
+
+#if __has_include(<flutter_native_splash/FlutterNativeSplashPlugin.h>)
+#import <flutter_native_splash/FlutterNativeSplashPlugin.h>
+#else
+@import flutter_native_splash;
+#endif
+
+#if __has_include(<image_picker_ios/FLTImagePickerPlugin.h>)
+#import <image_picker_ios/FLTImagePickerPlugin.h>
+#else
+@import image_picker_ios;
+#endif
+
+#if __has_include(<package_info_plus/FPPPackageInfoPlusPlugin.h>)
+#import <package_info_plus/FPPPackageInfoPlusPlugin.h>
+#else
+@import package_info_plus;
+#endif
+
+#if __has_include(<shared_preferences_foundation/SharedPreferencesPlugin.h>)
+#import <shared_preferences_foundation/SharedPreferencesPlugin.h>
+#else
+@import shared_preferences_foundation;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTFirebaseFirestorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseFirestorePlugin"]];
+  [FLTFirebaseFunctionsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseFunctionsPlugin"]];
+  [FLTFirebaseAnalyticsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAnalyticsPlugin"]];
+  [FLTFirebaseAppCheckPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAppCheckPlugin"]];
+  [FLTFirebaseAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAuthPlugin"]];
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [FLTFirebaseMessagingPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseMessagingPlugin"]];
+  [FLTFirebaseStoragePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseStoragePlugin"]];
+  [FlutterNativeSplashPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterNativeSplashPlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [FPPPackageInfoPlusPlugin registerWithRegistrar:[registry registrarForPlugin:@"FPPPackageInfoPlusPlugin"]];
+  [SharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"SharedPreferencesPlugin"]];
+}
+
+@end
