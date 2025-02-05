61ee6a60c8e39a2a429c4e8b43fa9913c53866f6
Zaki Nazzal <zakinazal112@gmail.com>
2025-02-05 19:03:48 +0800

    modified:   android/app/build.gradle 	modified:   android/gradle/wrapper/gradle-wrapper.properties 	modified:   android/settings.gradle 	modified:   lib/pages/explore_page.dart 	modified:   lib/pages/home_page.dart 	modified:   lib/pages/profile_page.dart 	modified:   lib/util/exercise_tile.dart 	modified:   pubspec.lock
    	modified:   android/app/build.gradle
    	modified:   lib/pages/home_page.dart
    	modified:   lib/util/emoticon_face.dart
    	modified:   lib/util/splash_screen.dart
    	modified:   lib/util/square_tile.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_super_parameters, library_private_types_in_public_api, use_key_in_widget_constructors, avoid_print, unused_element
+// ignore_for_file: use_super_parameters, library_private_types_in_public_api, use_key_in_widget_constructors, avoid_print, unused_element, deprecated_member_use
 
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously, use_super_parameters
+// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously, use_super_parameters, deprecated_member_use
 
 import 'package:exercise/pages/edit_profile.dart';
 import 'package:firebase_auth/firebase_auth.dart';
